f956ffebb7d8e9cfdd9914655ccc572b31f58e2f
HauptmanDev <MainVados@gmail.com>
2020-01-04 19:29:54 +0300

    Change InputContainer

--- src/components/TrainingInputContainer.tsx
+++ src/components/TrainingInputContainer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import TrainingInputFile from "./TrainingInputFile";
 
 const TrainingInputContainer: React.FC = () => {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div>
+            <TrainingInputFile/>
+        </div>
+    );
 };
 
 export default TrainingInputContainer;
