a590d9bca12fb7c966794625bd7aa871429a3248
HauptmanDev <MainVados@gmail.com>
2020-01-04 19:21:29 +0300

    Add structure app.

---
+++ src/components/TreningInputFile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const TreningInputFile: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default TreningInputFile;
---
+++ src/components/TreningInputPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TreningInputContainer from "./TreningInputContainer";
+
+const TreningInputPage: React.FC = () => {
+    return (
+        <div>
+            <TreningInputContainer/>
+        </div>
+    );
+};
+
+export default TreningInputPage;
