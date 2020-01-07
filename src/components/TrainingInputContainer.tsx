610f5657c3a2e65ae2183968fa664cb8e7ddcf78
ibulavsky <ibulavsky@gmail.com>
2020-01-07 20:03:17 +0300

    Added FileReader.

--- src/components/TrainingInputContainer.tsx
+++ src/components/TrainingInputContainer.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 // import './App.css';
-import InputSomeFile from "./audio/InputSomeFile";
+import TrainingInputFile from "./TrainingInputFile";
 
 const TrainingInputContainer: React.FC = () => {
     return (
         <div>
-            <InputSomeFile callbackFileData={(fileData, fileURL)=>{
-                // save data fileData, fileURL
-            }}/>
+            <TrainingInputFile/>
         </div>
     );
 };
--- src/components/audio/InputSomeFile.tsx
+++ src/components/audio/InputSomeFile.tsx
@@ -1,32 +1,41 @@
-import React, {ChangeEvent, useRef, useState} from 'react';
+import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
 
 interface IPropsSomeFile {
-    callbackFileData: (fileData: string, fileURL: string) => void
+    callbackFileData: (fileURL: string, fileData: string) => void
 }
 
 const InputSomeFile: React.FC<IPropsSomeFile> = ({callbackFileData}) => {
-
+//state
     const [file, saveFile] = useState();
     const [fileURL, saveFileURL] = useState();
     const [fileData, saveFileData] = useState();
 
+    // console.log(fileURL, fileBlobURL, fileData);
 
+    useEffect(() => {
+        callbackFileData(fileURL, fileData)
+    }, [fileURL, fileData]);
+
+    //ref
     const inputFile = useRef<HTMLInputElement>(null);
+
+//methods
     const onButtonClick = () => {
         // `current` указывает на смонтированный элемент `input`
         inputFile.current!.click();
     };
 
-
     const onSaveFile = (e: ChangeEvent<HTMLInputElement>) => {
-        // const reader = new FileReader();
+        const reader = new FileReader;
         const formData = new FormData();
         const newFile = e.target.files && e.target.files[0];
-        saveFile(newFile);
-        saveFileURL(window.URL.createObjectURL(newFile));
-        newFile && formData.append('image', newFile, newFile.name);
-        saveFileData(formData.get('image'));
-        callbackFileData(fileURL, fileData)
+        reader.onloadend = () => {
+            saveFile(newFile);
+            saveFileURL(window.URL.createObjectURL(newFile)); // blob-url
+            newFile && formData.append('file', newFile, newFile.name);
+            saveFileData(formData.get('file'));
+        };
+        newFile && saveFileURL(reader.readAsDataURL(newFile));
     };
 
     return (
